bc2affc2e25fef0d6b78e385629581079c012b6a
MerelMoehn <merelmoehn@outlook.com>
2022-09-17 18:23:08 +0000

    Feat: creation of two random numbers out of the allCards array

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,10 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
+
+function runGame() {
+    let allCards = [1, 2, 3, 11, 7, 8, 9, 10, 11];
+    let playerCard = allCards[Math.floor(Math.random() * allCards.length)];
+    let bankCard = allCards[Math.floor(Math.random() * allCards.length)];
+    console.log(playerCard, bankCard);
+}
