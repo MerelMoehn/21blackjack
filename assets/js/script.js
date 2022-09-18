1e9bc345226d43b99df794e071ca48fcfef24b54
MerelMoehn <merelmoehn@outlook.com>
2022-09-18 12:30:57 +0000

    Fix: adjust runGame() to faciliate the hiding of the computer score and making it visible when user holds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 //Wait for the DOM to finish laoding before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function(){
-        if (this.getAttribute("data-type") === "hit"){
-            runGame();
-        } else if (this.getAttribute("data-type") === "hold"){
-            alert("You clicked hold!")
-        } else {
-            alert("You clicked reset!")
-        }
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "hit") {
+                runGame("hit");
+            } else if (this.getAttribute("data-type") === "hold") {
+                runGame("hold")
+            } else {
+                alert("You clicked reset!")
+            }
         })
     }
 })
@@ -20,39 +20,35 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * the main game loop, which will start when the user clicks 'hit me!'
  */
-function runGame() {
+function runGame(selectedControl) {
 
     // creates two random numbers out of the possible cards/numbers of the 21 game
     let allCards = [1, 2, 3, 11, 7, 8, 9, 10, 11];
     let playerCard = allCards[Math.floor(Math.random() * allCards.length)];
     let bankCard = allCards[Math.floor(Math.random() * allCards.length)];
-    
-    let playerScore = parseInt(document.getElementById("playerScore").innerText);
-    if(playerScore < 21){
-        document.getElementById("playersCard").innerText = playerCard;
-        incrementPlayerScore(playerCard);
-    } else {
-        alert("You have lost, click the reset button to start again");
+    let bankScore = 0;
+
+    if (selectedControl === "hit") {
+        let playerScore = parseInt(document.getElementById("playerScore").innerText);
+        if (playerScore < 21) {
+            document.getElementById("playersCard").innerText = playerCard;
+            incrementPlayerScore(playerCard);
+            playBank(bankCard);
+        } else {
+            alert("You have lost, click the reset button to start again");
+        }
     }
 
-    let bankScore = parseInt(document.getElementById("bankScore").innerText);
-    if(bankScore < 17){
-        document.getElementById("bankCard").innerText = bankCard;
-        incrementBankScore(bankCard);
-    } else {
-        alert("The bank will hold");
-        bankCard = 0;
-        incrementBankScore(bankCard);
+    if (selectedControl === "hold") {
+        do {
+            bankScore = bankScore + bankCard;
+          }
+          while (bankScore < 17);
+        document.getElementById("bankScore").innerText = bankScore;
     }
-
 }
 
-function incrementPlayerScore(playerCard) {
-    let oldScore = parseInt(document.getElementById("playerScore").innerText);
-    document.getElementById("playerScore").innerText= oldScore + playerCard;
-}
-
-function incrementBankScore(bankCard) {
-    let oldScore = parseInt(document.getElementById("bankScore").innerText);
-    document.getElementById("bankScore").innerText= oldScore + bankCard;
-}
+    function incrementPlayerScore(playerCard) {
+        let oldScore = parseInt(document.getElementById("playerScore").innerText);
+        document.getElementById("playerScore").innerText = oldScore + playerCard;
+    }
